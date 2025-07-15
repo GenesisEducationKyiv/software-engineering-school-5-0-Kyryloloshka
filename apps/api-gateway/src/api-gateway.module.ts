dc3fa54012ad9e0c0f6c0d485f27e9055fca621a
Kyrylo Haliamov <kirill.contrmptuous@gmail.com>
2025-07-15 18:17:55 +0300

    feat(server): add dockerfiles to microservices

--- apps/api-gateway/src/api-gateway.module.ts
+++ apps/api-gateway/src/api-gateway.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { WeatherModule } from './weather/weather.module';
 import { SubscriptionModule } from './subscription/subscription.module';
+
 @Module({
   imports: [
     ConfigModule.forRoot({
